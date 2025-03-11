262b5a324ad86ad5a79c6ad62c96ecfadac82f44
Aryaman Shrestha <87716416+aryamanstha@users.noreply.github.com>
2025-03-11 15:15:05 +0545

    temperature parameter added

--- prompt_template/template.py
+++ prompt_template/template.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model = ChatOpenAI(model="gpt-4o-mini")
+model = ChatOpenAI(model="gpt-4o-mini", temperature=0.5)
 
 template = "Write me a paragraph about {topic}."
 prompt_template = ChatPromptTemplate.from_template(template)
 
-prompt = prompt_template.invoke({"topic": "Deep Learning"})
+prompt = prompt_template.invoke({"topic": "Machine Learning"})
 result = model.invoke(prompt)
 print(result.content)
